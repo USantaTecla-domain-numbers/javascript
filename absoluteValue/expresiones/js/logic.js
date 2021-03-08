ed2c5ca84b9ad766a46beea30a899bc135d1e38e
Setillo <setillofm@gmail.com>
2021-03-08 12:14:39 +0100

    refactor

--- absoluteValue/expresiones/js/logic.js
+++ absoluteValue/expresiones/js/logic.js
@@ -1,3 +1,3 @@
 let number = +prompt("Dame un número:");
-let absolute = number<0 ? number*-1 : number;
+let absolute = number < 0 ? number * -1 : number;
 alert("Su valor absoluto es:" + absolute);
--- palindrome/logic.js
+++ palindrome/logic.js
@@ -1,22 +1,19 @@
-{
-  //  Dado un número, determinar si es palíndromo 
-  function inverse(number) {
-    let inverso = 0;
-    do {
-      inverso = inverso * 10 + number % 10;
-      number = parseInt(number / 10);
-    } while (number > 0);
-    return inverso;
-  }
-
-  function isPalindrome(number) {
-    return number == inverse(number);
-  }
-
-  let number = 9876789;
-  let answer = "NO";
-  if (isPalindrome(number)) {
-    answer = "SI";
-  }
-  console.log("El numero " + number + " " + answer + " es palidromo");
+function inverse(number) {
+  let result = 0;
+  do {
+    result = result * 10 + number % 10;
+    number = parseInt(number / 10);
+  } while (number > 0);
+  return result;
 }
+
+function isPalindrome(number) {
+  return number == inverse(number);
+}
+
+let number = +prompt("Introduce un número: ");
+let answer = "NO";
+if (isPalindrome(number)) {
+  answer = "SI";
+}
+console.log("El numero " + number + " " + answer + " es palidromo");
